05eaa31e4ffcdbcb33437c755429a30c566c84c2
Dhananjay-JSR <ajaysenday@gmail.com>
2022-10-05 00:37:17 +0530

    Updated Login Page

--- frontend/my-app/pages/foodify/option.tsx
+++ frontend/my-app/pages/foodify/option.tsx
@@ -1,12 +1,14 @@
 import type {NextPage} from "next";
-import React, {useState} from "react";
+import React, {useState, useRef, MutableRefObject} from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const Option:React.FC = ()=>{
-    const [FoodList , setFoodList] = useState<Array<ListType>|null>()
-    const [foodTypeInput , setfoodTypeInput] = useState<string>()
-    const [QuantityInput , setQuantityInput] = useState<number>()
-    const [DRY_WETInput , setDRY_WETInput] = useState<'DRY'|'WET'|string>()
-    const [ApproximationInput , setApproximationInput] = useState<number>()
+    const FoodInputRef = useRef<HTMLInputElement>();
+    const [FoodList , setFoodList] = useState<Array<ListType>>([])
+    const [FoodTypeInput , setFoodTypeInput] = useState<string|null>()
+    const [QuantityInput , setQuantityInput] = useState<number|null>()
+    const [DRY_WETInput , setDRY_WETInput] = useState<'DRY'|'WET'|string|null>()
+    const [ApproximationInput , setApproximationInput] = useState<number|null>()
     interface ListType {
         foodType:string,
         Quantity:number,
@@ -66,45 +68,75 @@ const Option:React.FC = ()=>{
             </div>
         </div>
         {/*    ---------------------------------------------*/}
-        {FoodList?.map((e)=>{
+        {FoodList?.map((e,index)=>{
+            //TODO: FIX The Even occuring Key Prop before deploying
             return<>
-                <div key={e.foodType.toString()} style={{
+                <div key={uuidv4()} style={{
                     display: 'grid',
                     gridTemplateColumns: '230px 300px 200px auto'
                 }}>
-                    <div > {e.foodType} </div>
-                    <div > {e.Quantity} </div>
-                    <div > {e.DRY_WET} </div>
-                    <div > {e.Approximation}</div>
+                    <div key={uuidv4()} > {e.foodType} </div>
+                    <div key={uuidv4()} > {e.Quantity} </div>
+                    <div  key={uuidv4()}> {e.DRY_WET} </div>
+                    <div key={uuidv4()} > {e.Approximation}</div>
                 </div>
             </>
         })}
+        {/*    ---------------------------------------------*/}
+
         <div style={{
             display: 'grid',
-            gridTemplateColumns: '230px 300px 200px auto'
+            gridTemplateColumns: '230px 300px 200px auto',
+            margin: '10px'
         }}>
-            <input value={foodTypeInput} onChange={(e)=>{
-                setfoodTypeInput(prev=>e.target.value)
+            <input ref={FoodInputRef as MutableRefObject<HTMLInputElement>} placeholder={'Enter Food Name Here'} value={FoodTypeInput||''} onChange={(e)=>{
+                setFoodTypeInput(e.target.value)
             }} />
-            <input value={QuantityInput} onChange={(e)=>{
-                setQuantityInput(prev=>+(e.target.value))
+            <input value={QuantityInput||''} placeholder={"Enter Quantity here"} onChange={(e)=>{
+                setQuantityInput(+(e.target.value|| ''))
             }} type={'number'}/>
-            <input value={DRY_WETInput} onChange={(e)=>{
-                setDRY_WETInput(prev=>e.target.value)
+            <input value={DRY_WETInput || ''} placeholder={"Enter DRY or WET"} onChange={(e)=>{
+                setDRY_WETInput(e.target.value)
             }}/>
-            <input value={ApproximationInput} onChange={(e)=>{
-                    setApproximationInput(prev=>+e.target.value)
+            <input onKeyDown={(e)=>{
+            if(e.code==='Enter'){
+                if((FoodTypeInput!=undefined)&&(ApproximationInput!=undefined)&&(DRY_WETInput!=undefined)&&(QuantityInput!=undefined)){
+                    const FoodItem :ListType={
+                        foodType: FoodTypeInput,
+                        Approximation: ApproximationInput,
+                        DRY_WET: DRY_WETInput,
+                        Quantity: QuantityInput
+                    }
+                    setFoodList(prev=>[...prev,FoodItem])
+                    FoodInputRef.current?.focus();
+                    setFoodTypeInput(null)
+                    setQuantityInput( null)
+                    setDRY_WETInput(null)
+                    setApproximationInput(null)
+                }
+            }
+
+            }} value={ApproximationInput||''}  placeholder={'Enter Amount that could be eaten by old'}  onChange={(e)=>{
+
+                    setApproximationInput(+e.target.value)
             }}/>
         </div>
         <button onClick={()=>{
-            if((foodTypeInput!=undefined)&&(ApproximationInput!=undefined)&&(DRY_WETInput!=undefined)&&(QuantityInput!=undefined)){
-            const FoodItem :ListType={
-                foodType: foodTypeInput,
+            if((FoodTypeInput!=undefined)&&(ApproximationInput!=undefined)&&(DRY_WETInput!=undefined)&&(QuantityInput!=undefined)){
+                const FoodItem :ListType={
+                foodType: FoodTypeInput,
                 Approximation: ApproximationInput,
                 DRY_WET: DRY_WETInput,
                 Quantity: QuantityInput
             }
-            setFoodList(prev=>  prev!=undefined?[...prev,FoodItem]:null )
+            setFoodList(prev=>[...prev,FoodItem])
+                FoodInputRef.current?.focus();
+                setFoodTypeInput(null)
+                setQuantityInput( null)
+                setDRY_WETInput(null)
+                setApproximationInput(null)
+            }else{
+                window.alert("Empty Field Detected")
             }
         }}>
             Add More Data
